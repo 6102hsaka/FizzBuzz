7ee91bf0f9de12a50fad83cc8dc495041fb3024b
6012hsaka <akashsharma15105@gmail.com>
2020-09-21 14:26:30 +0530

    Test case for Multiple of 3 and 5 Passed

--- src/com/akash/FizzBuzzImpl.java
+++ src/com/akash/FizzBuzzImpl.java
@@ -3,6 +3,10 @@ package com.akash;
 public class FizzBuzzImpl {
 
 	static String getResult(int x) {
+		if (x % 3 == 0 && x % 5 == 0) {
+			return "FizzBuzz";
+		}
+
 		if (x % 3 == 0) {
 			return "Fizz";
 		}
