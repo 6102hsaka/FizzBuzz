7fe5eded3840055a0d32ef0fa884fff0cd645a95
6012hsaka <akashsharma15105@gmail.com>
2020-09-21 14:16:41 +0530

    Test case for 5 Passed

--- src/com/akash/FizzBuzzImpl.java
+++ src/com/akash/FizzBuzzImpl.java
@@ -3,8 +3,11 @@ package com.akash;
 public class FizzBuzzImpl {
 
 	static String getResult(int x) {
-		if(x%3==0) {
+		if (x % 3 == 0) {
 			return "Fizz";
+		}
+		if (x == 5) {
+			return "Buzz";
 		}
 		return Integer.toString(x);
 	}
